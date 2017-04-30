1201c50d9b8e6d3f88aacf9514cc8f6efb5432f4
RobCarney <robert.edward.carney@gmail.com>
2017-04-30 15:02:44 -0400

    Simplifying using a .mat dataset for k-means algorithm proof of concept

--- FunctionalityTest/ImplementationScript.py
+++ FunctionalityTest/ImplementationScript.py
@@ -1,58 +1,59 @@
-from PIL import Image
-
+import scipy.io as sp
 import numpy as np
-im = Image.open('bird_small.png')
 
-pix = im.load()
-data = np.asarray(im)
-im1 = Image.fromarray(data)
-im1.show()
 
-norm = data / 255;
-s = norm.shape
+data = sp.loadmat('kmeans_test_data.mat')['X']
 
-dim = s[0]*s[1];
 
-fData = np.reshape(norm,(dim,s[2]))
-
-def initCentroids(k, X):
-    randix = np.random.permutation(fData.shape[0])
-    randix = randix[0:k]
-    centroids = X[randix, :]
-    return centroids
+initial_centroids = np.asarray([[3,3],[6,2],[8,5]])
 
 def runKMeans(X, centroids, iters):
-
-
-    return;
+    (m,n) = X.shape
+    (K,l) = centroids.shape
+    idx = np.zeros((m,1))
+    for i in range(0,iters):
+        idx = findClosestCentroids(X,centroids)
+        centroids = computeCentroids(X,idx,K)
+    return (idx,centroids);
 
 def findClosestCentroids(X, centroids):
     (k,d) = centroids.shape
-    idx = np.zeros((k,1))
-    for i in range(0,X.shape[0]):
+    (m,n) = X.shape
+    idx = np.zeros((m,1))
+    for i in range(0,m):
         min = 1000000000000
         xr = X[i,:]
         best = 1
         for j in range(0,k):
-            p = centroids[:,j]
+            p = centroids[j,:]
             sub = np.subtract(p,xr)
             result = np.dot(sub,sub)
             if (result < min):
                 best = j
                 min = result
-        idx[i,1] = best
+        idx[i,0] = best
     return idx
 
-def computeCentroids(X, idx, K):
+def computeCentroids(X, idx,K):
     (m,n) = X.shape
-    centroids = np.zeros(K,n)
-
-
-    return
+    centroids = np.zeros((K,n))
+    for i in range(0,K):
+        countInt = (idx == i).sum().astype(int)
+        count = np.zeros((1,n))
+        res = np.zeros((1, n))
+        for p in range(0,n):
+            count[0,p] = countInt
+        for j in range(0,m):
+            if (idx[j,0]==i):
+                res = np.add(res, data[j,:])
+        centroids[i,:] = res / count
+    return centroids
 
 x = np.array([1, 2, 4])
 y = np.array([2, 4, 1])
 x[2] = 1
-print(x)
 
-##print(initCentroids(10,fData))
+idx = findClosestCentroids(data, initial_centroids)
+centroids = computeCentroids(data, idx, 3)
+
+

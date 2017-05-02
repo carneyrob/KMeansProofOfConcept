a9a31467c086125686cd3b01a307b2bfe841a5ea
RobCarney <robert.edward.carney@gmail.com>
2017-05-02 18:41:10 -0400

    Image work

--- FunctionalityTest/ImageCompImplementation.py
+++ FunctionalityTest/ImageCompImplementation.py
@@ -1,3 +1,32 @@
 import numpy as np
 from PIL import Image
 
+# For loading images as NP arrays
+def load_image( infilename ) :
+    img = Image.open( infilename )
+    img.load()
+    data = np.asarray( img, dtype="int32" )
+    return data
+
+# For saving NP arrays as images (given a file location)
+def save_image( npdata, outfilename ) :
+    img = Image.fromarray( np.asarray( np.clip(npdata,0,255), dtype="uint8"), "L" )
+    img.save( outfilename )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

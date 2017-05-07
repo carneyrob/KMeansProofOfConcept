6e9a1288deefe18639c2a87ae16a4bbfb5bd41e1
RobCarney <robert.edward.carney@gmail.com>
2017-05-07 17:36:04 -0400

    More refactor

--- FunctionalityTest/imageCompressor/ImageCompressor.py
+++ FunctionalityTest/imageCompressor/ImageCompressor.py
@@ -4,4 +4,6 @@ import numpy as np
 class ImageCompressor:
 
     def __init__(self, path):
-        self.image =
+        img = Image.open(path)
+        img.load()
+        self.image_as_array = np.asarray( img, dtype="float32" )
